fcad92aed5317e080c07f026ee23ab5dbc92edf9
hediehrafiee <hrafiee1995@gmail.com>
2019-07-18 17:18:34 +0430

    Modify router

--- src/router.js
+++ src/router.js
@@ -6,7 +6,8 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/vue-horizontally-slider/',
+  mode: 'history',
   routes: [
     {
       path: '/',
